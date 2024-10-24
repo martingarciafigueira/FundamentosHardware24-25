ba4c4a3e298bd895cbc0e8a434118076c1f10747
Martin <martin.garcia.figueira@ciclosmontecastelo.com>
2024-10-24 20:03:57 +0200

    Create Actividad4.py

---
+++ UD03-Raspberry Pi (Python)/Actividades/24-25/Actividad4.py
@@ -0,0 +1,13 @@
+# #Imprime los numeros del 1 al 10 con un bucle FOR
+for i in range(1,10+1,1):
+    print(i)
+
+# #Imprime los numeros del 10 al 1 con un bucle FOR
+for i in range(10,0,-1):
+    print(i)
+
+#Imprime los numeros del 10 al 1 con un bucle WHILE
+i = 10
+while i > 0:
+    print(i)
+    i = i-1
