31922aa6384ec653b80dc8088ebda5006654774d
Martin <martin.garcia.figueira@ciclosmontecastelo.com>
2024-10-24 18:02:17 +0200

    UD1

---
+++ UD03-Raspberry Pi (Python)/Actividades/24-25/Actividad1.py
@@ -0,0 +1,9 @@
+nombre = "Martín"
+numeroPie = 47.5
+deporteFavorito = "Fútbol"
+
+print("Mi nombre es " + nombre)
+print("Mi numero de pie es:", numeroPie)
+print("Mi deporte favorito es:" + deporteFavorito)
+
+print ("Me llamo ",nombre," calzo un",numeroPie," y me gusta mucho el",deporteFavorito)
